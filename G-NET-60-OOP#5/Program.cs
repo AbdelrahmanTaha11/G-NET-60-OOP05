e3d0eec39ac9cdbf6208b52acf03b5cfd91b6562
Abdelrahman Elhalby <abdoelhalby60@gmail.com>
2026-06-22 00:56:53 +0300

    Question01

--- G-NET-60-OOP#5/Program.cs
+++ G-NET-60-OOP#5/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Part01
+            #region Question01
+            /*
+             Q1 : What is an interface in C#? Why do we use interfaces 
+                  instead of depending on concrete classes directly?
+                  Mention at least three benefits of using interfaces.
+
+
+                // An interface in C# is a contract that defines a set of methods,
+                // properties, or events that a class must implement.
+                //
+                // We use interfaces instead of depending directly on concrete classes
+                // to make the code more flexible and maintainable.
+                //
+                // Benefits of using interfaces:
+                // 1. Loose Coupling: Reduces dependency between classes.
+                // 2. Flexibility: Different classes can implement the same interface.
+                // 3. Easy Testing: Makes unit testing and mocking easier.
+                // 4. Polymorphism: Allows working with different implementations
+                //    through a common interface reference.
+
+       
+             */
+
+
+
+            #endregion
+            #endregion
         }
     }
 }
